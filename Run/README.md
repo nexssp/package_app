c0897ca8d8cf039d6fd9033bc5ab1a1e4de48a3f
Marcin Polak <mapoart@gmail.com>
2020-03-07 17:41:10 +0100

    Changed from ahk to ps1. Now runs as parameters not attributes.

--- Run/README.md
+++ Run/README.md
@@ -2,14 +2,21 @@
 
 Run application/program
 
-## Examples
+# Examples (run.ps1)
+
+```sh
+nexss App/Run blender
+nexss App/Run explorer.exe
+```
+
+## Examples / Old Version (file run.ahk)
 
 ```sh
 nexss App/Run --app="code c:/myfolder"
 nexss App/Run --app="explorer"
 ```
 
-## Credits
+## Credits / Old Version (file run.ahk)
 
 Languages/Technologies used for this Nexss PROGRAMMER package:
 
---
+++ Run/src/run.ps1
@@ -0,0 +1,19 @@
+$nxsParameters = @("")
+$input | . "$($env:NEXSS_PACKAGES_PATH)/Nexss/Lib/NexssIn.ps1"
+
+$ErrorActionPreference = "Stop"
+
+$program, $attrs = $inFieldValue_1
+
+$NULL, $installApps = $attrs
+
+& $program @attrs 
+
+if ( $inFieldValue_1) {  
+    $NexssStdout | Add-Member -Force -NotePropertyMembers  @{"$resultField_1" = $installApps }
+}
+else {
+    $NexssStdout.PSObject.Properties.Remove($resultField_1)   
+}
+
+. "$($env:NEXSS_PACKAGES_PATH)/Nexss/Lib/NexssOut.ps1"
